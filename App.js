cd4264f95b9d308e26f015660ad29545b05c5b56
bqcount <bqcount@gmail.com>
2024-07-08 11:51:50 +0100

    save settings, and confirm orders done

--- App.js
+++ App.js
@@ -4,14 +4,17 @@ import Main from "./src/components/Main";
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import SettingsDrawer from "./src/components/SettingsDrawer";
+import { AuthProvider } from "./src/context/AuthContext";
 
 const Stack = createStackNavigator();
 
 function App() {
   return (
+  <AuthProvider>
     <NavigationContainer>
       <SettingsDrawer />
     </NavigationContainer>
+  </AuthProvider>
   );
 }
 
